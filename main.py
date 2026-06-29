0b15db3e6e52592eedcab45c15882d12d5deda0c
betulemeksiz9-spec <betulemeksiz9@gmail.com>
2026-06-29 21:32:06 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "GHOSTİNG":"bir kişiyi görmezden gelmek",
+            "LOVE BOMBİNG":"BİR KİŞİYİ ÇOK İLGİYE BOĞUP ARDINDAN İLGİ GÖSTERMEYİ BIRAKMAK",
+            
+            }
+
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+    
+else:
+    print("doğru yazdığına emin misin")
